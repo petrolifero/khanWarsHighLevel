80d92c80f4037a6eab983f501e530fa4e3291eb1
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2021-08-09 05:07:57 -0300

    create actual diagram

--- diagrams/makeDiagram.sh
+++ diagrams/makeDiagram.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/bash
 
 pip install hpp2plantuml
+
 for header in `find .. -type f -iname "*hpp"`
 do
     includeOptions="$includeOptions -i $header"
 done
 hpp2plantuml $includeOptions -o actual.puml
+plantuml actual.puml
