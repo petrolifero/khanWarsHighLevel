4766759ed664c49a9db62e989feb0894e23f49a5
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2022-08-08 07:38:26 -0300

    refactor folder structure to ddd

--- domain/user/user.hpp
+++ domain/user/user.hpp
@@ -14,9 +14,6 @@ private:
 public:
   user(environmentVariableAuth auth,khanWarsAPI api);
   user(const user& other) = default;
-  //async
   void print(media m);
-  //async
-  void apply(changeCollection cc);
 };
 #endif
